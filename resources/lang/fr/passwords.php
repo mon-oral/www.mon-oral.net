12e0cd9016099d38b41971235183fc6cb96b960c
laurentabbal <laurent@abbal.com>
2021-08-23 12:48:47 +0900

    liens sidebar

--- resources/lang/fr/passwords.php
+++ resources/lang/fr/passwords.php
@@ -13,7 +13,7 @@ return [
     */
 
     'reset'     => 'Votre mot de passe a été réinitialisé !',
-    'sent'      => 'Nous vous avons envoyé par email le lien de réinitialisation du mot de passe (attendez quelques minutes avant de vérifier votre boîte aux lettres).',
+    'sent'      => 'Nous vous avons envoyé par courriel le lien de réinitialisation du mot de passe (attendez quelques minutes avant de vérifier votre boîte aux lettres).',
     'throttled' => 'Veuillez patienter avant de réessayer.',
     'token'     => "Ce jeton de réinitialisation du mot de passe n'est pas valide.",
     'user'      => "Aucun utilisateur n'a été trouvé avec cette adresse email.",
--- resources/views/inc-sidebar.blade.php
+++ resources/views/inc-sidebar.blade.php
@@ -1,53 +1,38 @@
-<!--
-<div class="mt-4 mb-5 text-center">
-	<a class="btn btn-outline-secondary btn-sm" href="https://monoral.flarum.cloud" role="button">discussions</a>
-	<a class="btn btn-outline-secondary btn-sm" href="https://docs.mon-oral.net" role="button">guides</a>
-</div>
+<table>
+	<tr>
+		<td style="width:90%"><a href="/console/activites" role="button" class="btn btn-light text-left text-muted" style="width:100%" data-toggle="tooltip" data-placement="top" title="afficher les activités audio"><span class="small">activités</span></a></td>
+		<td><a href="/console/activite-creer" role="button" class="btn btn-light text-dark" data-toggle="tooltip" data-placement="top" title="créer une activité audio pour les élèves (récitation, lecture expressive, description orale...)"><i class="fas fa-plus fa-xs"></i></a></td>
+	</tr>
+	<tr>
+		<td style="width:90%"><a href="/console/commentaires" role="button" class="btn btn-light text-left text-muted" style="width:100%" data-toggle="tooltip" data-placement="top" title="afficher les commentaires audio"><span class="small">commentaires</span></a></td>
+		<td><a href="/console/commentaire-creer" role="button" class="btn btn-light text-dark" data-toggle="tooltip" data-placement="top" title="créer des commentaires audio (correction orale de copies, consignes orales, explications...)"><i class="fas fa-plus fa-xs"></i></a></td>
+	</tr>
+	<tr>
+		<td style="width:90%"><a href="/console/entrainements" role="button" class="btn btn-light text-left text-muted" style="width:100%" data-toggle="tooltip" data-placement="top" title="afficher les entraînements de type examen"><span class="small">entraînements</span></a></td>
+		<td><a href="/console/entrainement-creer" role="button" class="btn btn-light text-dark" data-toggle="tooltip" data-placement="top" title="créer un entraînement de type examen (EAF, Grand Oral, langue, brevet...)"><i class="fas fa-plus fa-xs"></i></a></td>
+	</tr>
 
+</table>
 
--->
+<a data-toggle="collapse" role="button" href="#conseils" class="mt-4 btn btn-light text-center btn-sm" style="width:100%" aria-expanded="false" aria-controls="conseils">
+	<span class="text-danger small">CONSEILS ET REMARQUES</span>
+</a>
 
-<div style="clear:both">
-	<div class="btn-toolbar mb-1" role="toolbar">
-		<div class="btn-group" role="group" style="width:100%">
-			<a href="/console/entrainements" role="button" class="btn btn-light text-left text-muted" style="width:100%"><span class="small">entraînements</span></a>
-			<a href="/console/entrainement-creer" role="button" class="btn btn-light text-dark" data-toggle="tooltip" data-placement="top" title="créer un entraînement"><i class="fas fa-plus fa-xs"></i></a>
-		</div>
-	</div>
-</div>
+<a href="https://github.com/mon-oral/www.mon-oral.net/discussions" target="_blank" role="button" class="mt-4 btn btn-light btn-sm text-left text-muted" style="width:100%;opacity:0.8;" data-toggle="tooltip" data-placement="top" title="remarque : un compte GitHub est nécessaire">
+	<span style="font-size:90%"><i class="fas fa-comment-alt" style="float:left;margin:4px 8px 5px 0px;"></i> discussions <span style="opacity:0.6;font-size:90%;">&</span> annonces</span>
+</a>
 
-<div>
-	<div class="btn-toolbar mb-1" role="toolbar">
-		<div class="btn-group" role="group" style="width:100%">
-			<a href="/console/activites" role="button" class="btn btn-light text-left text-muted" style="width:100%"><span class="small">activités</span></a>
-			<a href="/console/activite-creer" role="button" class="btn btn-light text-dark" data-toggle="tooltip" data-placement="top" title="créer une activité audio"><i class="fas fa-plus fa-xs"></i></a>
-		</div>
-	</div>
-</div>
-
-<div style="clear:both">
-	<div class="btn-toolbar mb-1" role="toolbar">
-		<div class="btn-group" role="group" style="width:100%">
-			<a href="/console/commentaires" role="button" class="btn btn-light text-left text-muted" style="width:100%"><span class="small">commentaires</span></a>
-			<a href="/console/commentaire-creer" role="button" class="btn btn-light text-dark" data-toggle="tooltip" data-placement="top" title="créer un commentaire audio"><i class="fas fa-plus fa-xs"></i></a>
-		</div>
-	</div>
-</div>
-
-<a data-toggle="collapse" role="button" href="#conseils" class="mt-4 btn btn-light text-center btn-sm" style="width:100%" aria-expanded="false" aria-controls="conseils"><span class="text-danger small">CONSEILS ET REMARQUES</span></a>
-
-<!--
-<a href="https://monoraldotnet.flarum.cloud/" target="_blank" role="button"  class="mt-4 btn btn-light text-center btn-sm text-muted" style="width:100%" aria-expanded="false" aria-controls="conseils"><span style="font-size:90%">forum</span></a>
-<a href="https://wiki.mon-oral.net" target="_blank" role="button"  class="mt-1 btn btn-light text-center btn-sm text-muted" style="width:100%" aria-expanded="false" aria-controls="conseils"><span style="font-size:90%">wiki</span></a>
--->
+<a href="https://github.com/mon-oral/www.mon-oral.net/issues/new/choose" target="_blank" role="button"  class="mt-1 btn btn-light text-left btn-sm text-muted" style="width:100%;opacity:0.8;" data-toggle="tooltip" data-placement="top" title="remarque : un compte GitHub est nécessaire">
+	<span style="font-size:90%"><i class="fas fa-bug" style="float:left;margin:4px 8px 5px 0px;"></i> signalement de bogues <span style="opacity:0.6;font-size:90%;">&</span> questions techniques</span>
+</a>
 
 <div class="text-monospace text-center small mt-4 mt-5 mb-5" style="color:silver">
 	contact : <a href="mailto:contact@mon-oral.net"><i class="fa fa-envelope text-info"></i></a>
 </div>
 
-<table class="mt-5 mt-5 mb-5 text-monospace small text-muted">	
+<table class="mt-5 mt-5 mb-5 text-monospace small text-muted">
 	<tr>
 		<td class="text-center pr-2 align-top pt-1 text-danger"><i class="fas fa-trash"></i></td>
 		<td class="pt-1 supprimer"><a tabindex="0" role="button" style="cursor:pointer;" data-trigger="focus" data-container="body" data-placement="right" data-toggle="popover" data-html="true" data-content="<a class='btn btn-danger btn-sm' href='{{ route('supprimer') }}' role='button'>confirmer</a> <a tabindex='0' class='btn btn-secondary btn-sm text-light' role='button'>annuler</a>">supprimer ce compte </a></td>
-	</tr>							
+	</tr>
 </table>
