3c325379d49ab831b369ece52ec0b8a1b8e118e9
laurentabbal <laurent@abbal.com>
2022-11-16 19:51:56 +0900

    lien mastodon

--- resources/views/inc-footer.blade.php
+++ resources/views/inc-footer.blade.php
@@ -1,7 +1,7 @@
 <div class="container-fluid pb-3" style="margin-top:200px;">
 	<div class="row">
 		<div class="col-md-12 text-center small">
-			<a href="{{ route('donnees-personnelles') }}">données personnelles</a> | <a href="mailto:contact@mon-oral.net">contact courriel</a> | illustrations : <a href="https://streamlinehq.com" target="_blank">streamlinehq.com</a> | <a href="https://twitter.com/laurentabbal" target="_blank" class="text-monospace">@laurentabbal</a> | <a href="https://twitter.com/mon_oral" target="_blank" class="text-monospace">@mon_oral</a>
+			<a href="{{ route('donnees-personnelles') }}">données personnelles</a> | <a href="mailto:contact@mon-oral.net">contact courriel</a> | illustrations : <a href="https://streamlinehq.com" target="_blank">streamlinehq.com</a> | <a href="https://twitter.com/laurentabbal" target="_blank" class="text-monospace">@laurentabbal</a> | <a href="https://twitter.com/mon_oral" target="_blank" class="text-monospace">@mon_oral</a> | <a rel="me" href="https://mastodon.social/@laurentabbal" target="_blank" class="text-monospace">@laurentabbal@mastodon.social</a>
 		</div>
 	</div>
 </div>
