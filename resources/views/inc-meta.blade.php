5a28d9b3502e9f5a40c9e58e4377b1c9f34fb42b
Laurent Abbal <laurent@abbal.com>
2023-02-11 12:10:36 +0900

    ~

--- resources/views/inc-meta.blade.php
+++ resources/views/inc-meta.blade.php
@@ -8,11 +8,11 @@
 <meta name="csrf-token" content="{{ csrf_token() }}">
 
 <!-- Font Awesome -->
-<link href="{{ asset('lib/fontawesome/css/all.css') }}" rel="stylesheet">
+<link rel="preload" href="{{ asset('lib/fontawesome/css/all.css') }}" rel="stylesheet">
 
 <!-- Styles -->
-<link href="{{ asset('css/app.css') }}" rel="stylesheet">
-<link href="{{ asset('css/custom.css') }}" rel="stylesheet">
+<link rel="preload" href="{{ asset('css/app.css') }}" rel="stylesheet">
+<link rel="preload" href="{{ asset('css/custom.css') }}" rel="stylesheet">
 
 <!-- Matomo -->
 <script type="text/javascript">
