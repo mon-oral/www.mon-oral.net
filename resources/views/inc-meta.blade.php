3bf2a770dcb720e6cac31a0a024930ae12c6be65
Laurent Abbal <laurent@abbal.com>
2023-02-11 13:38:03 +0900

    ~

--- resources/views/inc-meta.blade.php
+++ resources/views/inc-meta.blade.php
@@ -8,7 +8,9 @@
 <meta name="csrf-token" content="{{ csrf_token() }}">
 
 <!-- Font Awesome -->
-<link href="{{ asset('lib/fontawesome/css/all.css') }}" rel="stylesheet">
+<link href="{{ asset('lib/fontawesome/css/fontawesome.css') }}" rel="stylesheet">
+<link href="{{ asset('lib/fontawesome/css/solid.css') }}" rel="stylesheet">
+<link href="{{ asset('lib/fontawesome/css/brands.css') }}" rel="stylesheet">
 
 <!-- Styles -->
 <link href="{{ asset('css/app.css') }}" rel="stylesheet">
